9de294cccded63f9db543c504c8205e7df1d1a82
Dee9975 <matisskalnins6@gmail.com>
2019-10-29 12:17:53 +0200

    Comments

--- main.cpp
+++ main.cpp
@@ -3,16 +3,25 @@
 #include "Aita.h"
 
 int main() {
+  /**
+   * Bazes klasei virtualas funkcijas neko nemaina
+   */
   Dzivnieks *dz = new Dzivnieks;
   dz->setVards("Tofiks");
   dz->est("Baribu");
   dz->izdotSkanu();
 
+  /**
+   * Atvasinataja klase tiek parrakstitas funkcijas
+   */
   Aita *a = new Aita;
   a->est("sienu");
   a->izdotSkanu();
   a->dotVilnu();
 
+  /**
+   * Bazes klase, kas izmanto atvasinatas klases funkcijas
+   */
   Dzivnieks *va = new Aita;
   va->izdotSkanu();
   va->est("Salmi");
